d70e2fd1ca1f2f46d4c5e56e76542e5fd10710ab
Lucas Eduardo <lucased18@gmail.com>
2025-01-27 10:12:03 -0300

    feat
    criado projeto com header, style e homepage inicial

---
+++ project-root/src/App.jsx
@@ -0,0 +1,21 @@
+import Home from './components/Home';
+import './styles/App.css';
+
+function App() {
+
+  return (
+    <>
+      <section className="container-app">
+        <header>
+          <h1>Shortly</h1>
+        </header>
+        
+        <main>
+          <Home/>
+        </main>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ project-root/src/components/Home.jsx
@@ -0,0 +1,36 @@
+import '../styles/Home.css';
+import imgWork from '../images/illustration-working.svg';
+
+function Home() {
+    return (
+        <section className="wrap-home">
+            <section className="wrap-top">
+                <section className="wrap-top-left">
+                    <h2>Encurtador de URL</h2>
+                    <p>
+                        Simplifique seus links. Gere links curtos, fáceis de lembrar e de compartilhar.
+                    </p>
+                </section>
+                <img
+                    className='img-work' 
+                    src={imgWork} 
+                    alt="Illustration of a person working on a computer" 
+                />
+            </section>
+
+            <search className="search">
+                <input
+                    className="search-field"
+                    type="search"
+                    placeholder="Cole o link aqui..."
+                />
+
+                <button className="search-btn" type="submit">
+                    Encurtar URL
+                </button>
+            </search>
+        </section>
+    );
+}
+
+export default Home;
