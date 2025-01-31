4a4b1ec87d54fde638557dacc96b79719ccdb924
Lucas Eduardo <lucased18@gmail.com>
2025-01-31 10:55:53 -0300

    feat
    organizando o scss e separando componentes

--- project-root/src/components/Home.jsx
+++ project-root/src/components/Home.jsx
@@ -1,38 +1,12 @@
 import { useState } from 'react';
-import axios from 'axios'; // Importamos axios para fazer requisições HTTP
 
 import '../styles/Home.css';
 import imgWork from '../images/illustration-working.svg';
 import ShortenedUrlsList from './ShortenedUrlsList';
+import UrlField from './UrlField';
 
 function Home() {
-    // Estado para controlar o valor do campo de busca
-    const [inputValue, setInputValue] = useState('');
-    // Estado para controlar a exibição da mensagem de erro
-    const [error, setError] = useState(false);
     const [shortenedUrls, setShortenedUrls] = useState([]); // Novo estado para armazenar as URLs encurtadas
-    const [loading, setLoading] = useState(false);
-
-    // Função para lidar com o clique no botão
-    const handleShortenUrl = async () => {
-        if (inputValue.trim() === '') {
-            setError(true); // Mostra a mensagem de erro
-        } else {
-            setError(false); // Oculta a mensagem de erro
-            setLoading(true);
-            try {
-                const response = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(inputValue)}`);
-                
-                setShortenedUrls([...shortenedUrls, { original: inputValue, shortened: response.data }]);
-                setInputValue('');
-            } catch (error) {
-                console.error('Erro ao encurtar URL:', error.message);
-                setError(true);
-            } finally {
-                setLoading(false);
-            }
-        }
-    };
 
     const handleCopyUrl = (original, shortened) => {
         navigator.clipboard.writeText(shortened);
@@ -56,33 +30,11 @@ function Home() {
                     />
                 </section>
 
-                <section className="url">
-                    <section className="url-field-container">
-                        <input
-                            className={`url-field ${error ? 'error-border' : ''}`}
-                            id="url-field"
-                            type="text"
-                            placeholder="Cole o link aqui..."
-                            value={inputValue}
-                            onChange={(e) => setInputValue(e.target.value)} // Atualiza o estado com o valor digitado
-                        />
-                        {/* Exibe a mensagem de erro se o estado "error" for true */}
-                        {error && (
-                            <span className="error-message">
-                                Por favor, adicione um link
-                            </span>
-                        )}
-                    </section>
-
-                    <button 
-                        className="url-btn" 
-                        type="submit" 
-                        onClick={handleShortenUrl} 
-                        disabled={loading}
-                    >
-                        {loading ? 'Encurtando...' : 'Encurtar'}
-                    </button>
-                </section>
+                <UrlField 
+                    onShorten={(newUrl) => {
+                        setShortenedUrls(prevUrls => [...prevUrls, newUrl]);
+                    }}
+                />
             </section>
             <ShortenedUrlsList urls={shortenedUrls} onCopy={handleCopyUrl}/>
         </>
---
+++ project-root/src/components/UrlField.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import axios from 'axios';
+import '../styles/UrlField.css';
+import PropTypes from 'prop-types';
+
+const UrlField = ({ onShorten }) => {
+    // Prop types validation
+    UrlField.propTypes = {
+        onShorten: PropTypes.func.isRequired,
+    };
+
+    // Estado para controlar o valor do campo de busca
+    const [inputValue, setInputValue] = useState('');
+    // Estado para controlar a exibição da mensagem de erro
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+        setError(false);
+    };
+
+    // Função para lidar com o clique no botão
+    const handleShortenUrl = async () => {
+        if (inputValue.trim() === '') {
+            setError(true);
+        } else {
+            setError(false);
+            setLoading(true);
+            try {
+                const response = await axios.get(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(inputValue)}`);
+
+                onShorten({ original: inputValue, shortened: response.data });
+                setInputValue('');
+            } catch (error) {
+                console.error('Erro ao encurtar URL:', error.message);
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        }
+    };
+
+    return (
+        <section className="url">
+            <section className="url-field-container">
+                <input
+                    className={`url-field ${error ? 'error-border' : ''}`}
+                    id="url-field"
+                    type="text"
+                    placeholder="Cole o link aqui..."
+                    value={inputValue}
+                    onChange={handleInputChange}
+                />
+                {error && (
+                    <span className="error-message">
+                        Por favor, adicione um link
+                    </span>
+                )}
+            </section>
+            
+            <button
+                className="url-btn"
+                type="submit"
+                onClick={handleShortenUrl}
+                disabled={loading}
+            >
+                {loading ? 'Encurtando...' : 'Encurtar'}
+            </button>
+        </section>
+    );
+};
+
+export default UrlField;
