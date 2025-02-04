490e7a8608edab06aff176b1186f933329bdc15f
Lucas Eduardo <lucased18@gmail.com>
2025-02-04 13:47:46 -0300

    feat
    Add suporte ao modo escuro

--- project-root/src/App.jsx
+++ project-root/src/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import Home from './components/Home';
 import './styles/App.css';
+import { MdOutlineWbSunny } from "react-icons/md";
+import { IoMdMoon } from "react-icons/io";
 
 function App() {
   const handleRefresh = () => {
     window.location.reload();
   };
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle('dark-mode');
+  };
+
+  const modeIcon = isDarkMode ? <MdOutlineWbSunny />
+  : <IoMdMoon />;
+
+
   return (
     <>
       <section className="container-app">
         <header>
           <h1 onClick={handleRefresh}>Shortly</h1>
+          <button onClick={toggleDarkMode} className="toggle-button">{modeIcon}</button>
         </header>
         
         <main>
